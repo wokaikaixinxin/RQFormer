36cf1a81b9b8b0d946ba735bfa19d5163b8e997f
wokaikaixinxin <961211925@qq.com>
2025-06-03 10:52:32 +0800

    退回到之前的版本

--- projects/RQFormer/rroiformer/oriented_ddq_rcnn.py
+++ projects/RQFormer/rroiformer/oriented_ddq_rcnn.py
@@ -3,7 +3,6 @@ from mmdet.models.detectors.two_stage import TwoStageDetector
 from mmdet.models.utils.misc import unpack_gt_instances
 from mmengine.structures import InstanceData
 
-
 @MODELS.register_module()
 class OrientedDDQRCNN(TwoStageDetector):
     def loss(self,
@@ -20,10 +19,13 @@ class OrientedDDQRCNN(TwoStageDetector):
 
         losses = dict()
         x = self.extract_feat(batch_inputs)
+        # remove p2 for rpn
+        rpn_x = x[1:]
+        roi_x = x
 
         rpn_losses, imgs_whwht, distinc_query_dict, position_embed = \
             self.rpn_head.loss_and_predict(
-                x,
+                rpn_x,
                 batch_img_metas,
                 gt_bboxes,
                 gt_labels)
@@ -44,7 +46,7 @@ class OrientedDDQRCNN(TwoStageDetector):
             rpn_results_list.append(rpn_results)
 
         roi_losses = self.roi_head.loss(
-            x, rpn_results_list, batch_data_samples)
+            roi_x, rpn_results_list, batch_data_samples)
         losses.update(roi_losses)
 
         return losses
@@ -52,17 +54,20 @@ class OrientedDDQRCNN(TwoStageDetector):
     def predict(self,
                 batch_inputs,
                 batch_data_samples,
-                rescale=True):
+                rescale = True):
 
         outputs = unpack_gt_instances(batch_data_samples)
         batch_gt_instances, batch_gt_instances_ignore, batch_img_metas \
             = outputs
 
         x = self.extract_feat(batch_inputs)
+        # remove p2 for rpn
+        rpn_x = x[1:]
+        roi_x = x
 
         rpn_losses, imgs_whwht, distinc_query_dict, position_embed = \
             self.rpn_head.predict(
-                x, batch_img_metas)
+                rpn_x, batch_img_metas)
 
         proposals = distinc_query_dict['proposals']
         object_feats = distinc_query_dict['object_feats']
@@ -77,13 +82,14 @@ class OrientedDDQRCNN(TwoStageDetector):
             rpn_results.query_pos = position_embed.weight.clone()
             rpn_results_list.append(rpn_results)
 
-        results_list = self.roi_head.predict(x,
+        results_list = self.roi_head.predict(roi_x,
                                              rpn_results_list,
                                              batch_data_samples,
                                              rescale=rescale)
         batch_data_samples = self.add_pred_to_datasample(
             batch_data_samples, results_list)
         return batch_data_samples
+
 
     def _forward(self, batch_inputs, batch_data_samples) -> tuple:
         """Network forward process. Usually includes backbone, neck and head
@@ -106,10 +112,13 @@ class OrientedDDQRCNN(TwoStageDetector):
             = outputs
 
         x = self.extract_feat(batch_inputs)
+        # remove p2 for rpn
+        rpn_x = x[1:]
+        roi_x = x
 
         rpn_losses, imgs_whwht, distinc_query_dict, position_embed = \
             self.rpn_head.predict(
-                x, batch_img_metas)
+                rpn_x, batch_img_metas)
 
         proposals = distinc_query_dict['proposals']
         object_feats = distinc_query_dict['object_feats']
@@ -124,8 +133,8 @@ class OrientedDDQRCNN(TwoStageDetector):
             rpn_results.query_pos = position_embed.weight.clone()
             rpn_results_list.append(rpn_results)
 
-        roi_outs = self.roi_head.forward(x, rpn_results_list,
+        roi_outs = self.roi_head.forward(roi_x, rpn_results_list,
                                          batch_data_samples)
-        results = results + (roi_outs,)
+        results = results + (roi_outs, )
 
         return results
